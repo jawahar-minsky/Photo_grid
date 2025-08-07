dd7fa631011815fceb601155d252516518d5bd44
jawahar-minsky <218769701+jawahar-minsky@users.noreply.github.com>
2025-08-07 05:00:14 +0000

    Photo-grid assignment

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className = "container">
+      <div className = 'item item1'><h1>Photo Grid Assignment</h1><h2>By Jawahar</h2></div>
+      <div className='item'><img src="https://images.unsplash.com/photo-1749740587119-d3d7d9b9a19f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+      <div className='item'><img src="https://images.unsplash.com/photo-1743745330370-fb2c0c53fc9f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+      <div className='item'><img src="https://images.unsplash.com/photo-1749740577807-e20202e60da4?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+      <div className='item'><img src="https://images.unsplash.com/photo-1751677524360-978c93b2c0b2?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+      <div className='item'><img src="https://images.unsplash.com/photo-1682687220640-9d3b11ca30e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"/></div>
+      <div className = "item"><img src="https://images.unsplash.com/photo-1754147388611-c0f0179a05b5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+      <div className = "item"><img src="https://plus.unsplash.com/premium_photo-1754337885588-02a3ee7d1e51?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+      <div className = "item"><img src="https://images.unsplash.com/photo-1754147388611-c0f0179a05b5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div>
+    </div>
+  )
+}
+
+export default App
